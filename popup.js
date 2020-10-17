c4490af1c69dd8a650af5a124f9ce4f3b1f6f7a9
wytan01 <wytan@purdue.edu>
2020-10-18 04:06:47 +0800

    added chrome alarm and notifications API

--- popup.js
+++ popup.js
@@ -1,5 +1,13 @@
-$(function(){
-    $('#time').keyup(function(){
-        $('#greet').text('Posture check in ' + $('#time').val());
-    })
-})
+document.getElementById("add").addEventListener("click",remind);
+
+function remind(){
+    const minutes = parseInt(document.getElementById("minute").value);
+    const hours = parseInt(document.getElementById("hour").value);
+
+    if (isNaN(minutes) || isNaN(hours)){
+        console.log("It's not a number!");
+    }else{
+        console.log(minutes);
+        console.log(hours);
+    }
+}
